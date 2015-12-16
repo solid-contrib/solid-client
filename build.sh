c81747f7e9c92636350cc5783ee8f648c8aefe9a
deiu <andrei@fcns.eu>
2015-12-16 01:21:47 -0500

    Updated build script

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 # Build the dist file based on individual modules
+# TODO: minify dist file
 
-sources="solid.js auth.js identity.js status.js utils.js web.js"
+sources="auth.js identity.js status.js utils.js web.js"
+target="solid.js"
 dist="dist/solid.js"
 
-echo > $dist
+echo > $target
 
 for src in "$sources"
 do
-  cat $src >> $dist
+  cat $src >> $target
 done
+
+cp $target $dist
 
 echo "\nBuilt dist file in $dist\n"
 exit 0
