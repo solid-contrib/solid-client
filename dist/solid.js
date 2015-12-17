96a1637e7e621a0c65fb75aa8cb56a78db3ea499
deiu <andrei@fcns.eu>
2015-12-17 10:50:54 +0100

    Added getWorkspaces() function

--- dist/solid.js
+++ dist/solid.js
@@ -140,9 +140,11 @@ Solid.identity = (function(window) {
     'use strict';
 
     // common vocabs
+    var RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
     var OWL = $rdf.Namespace("http://www.w3.org/2002/07/owl#");
     var PIM = $rdf.Namespace("http://www.w3.org/ns/pim/space#");
     var FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
+    var DCT = $rdf.Namespace("http://purl.org/dc/terms/");
 
     // fetch user profile (follow sameAs links) and return promise with a graph
     // resolve(graph)
@@ -226,18 +228,40 @@ Solid.identity = (function(window) {
     };
 
     // Find the user's workspaces
+    // Return an object with the list of objects (workspaces)
     var getWorkspaces = function(webid, graph) {
         var promise = new Promise(function(resolve, reject){
             if (!graph) {
-                // fetch profile
+                // fetch profile and call function again
                 getProfile(webid).then(function(g) {
-                    return getWorkspaces(webid, g);
+                    getWorkspaces(webid, g).then(function(ws) {
+                        return resolve(ws);
+                    }).catch(function(err) {
+                        return reject(err);
+                    });
                 }).catch(function(err){
-                    reject(err);
+                    return reject(err);
                 });
             } else {
                 // find workspaces
-                console.log(graph);
+                var workspaces = [];
+                var ws = graph.statementsMatching($rdf.sym(webid), PIM('workspace'), undefined);
+                console.log(ws);
+                if (ws.length === 0) {
+                    return resolve(workspaces);
+                }
+                ws.forEach(function(w){
+                    // try to get some additional info - i.e. desc/title
+                    var workspace = {};
+                    var title = graph.any(w.object, DCT('title'));
+                    if (title && title.value) {
+                        workspace.title = title.value;
+                    }
+                    workspace.url = w.object.uri;
+                    workspace.statements = graph.statementsMatching(w.object, undefined, undefined);
+                    workspaces.push(workspace);
+                });
+                resolve(workspaces);
             }
         });
 
