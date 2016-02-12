155463c3890ce69bd7969b6b5893bb8daafae0b1
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-02-11 20:29:38 -0500

    Add some unit tests for SolidResponse object

---
+++ test/unit/solid-response-test.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var test = require('tape')
+var SolidResponse = require('../../lib/solid-response')
+
+test('empty SolidResponse test', function (t) {
+  t.plan(4)
+  let response = new SolidResponse()
+  t.notOk(response.xhr, 'An empty response should have an empty .xhr property')
+  t.notOk(response.user, 'An empty response should not have the user set')
+  t.notOk(response.isLoggedIn(), 'User should not be logged in')
+  t.notOk(response.exists(), 'Resource should not exist for empty header')
+})
+
+test('SolidResponse user.isLoggedIn test', function (t) {
+  t.plan(1)
+  let response = new SolidResponse()
+  response.user = 'https://localhost/profile/card#me'
+  t.ok(response.isLoggedIn(), 'User should be logged in')
+})
