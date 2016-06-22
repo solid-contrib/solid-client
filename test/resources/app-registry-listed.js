543718ba75eadac5b864eee5bd435722c2afb3a7
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-06-22 14:48:58 -0400

    Add app registry files

---
+++ test/resources/app-registry-listed.js
@@ -0,0 +1,9 @@
+/**
+ * Sample public AppRegistry resource
+ */
+module.exports = `@prefix solid: <http://www.w3.org/ns/solid/terms#>.
+
+<>
+  a solid:AppRegistry;
+  a solid:ListedDocument.
+`
---
+++ test/resources/app-registry-unlisted.js
@@ -0,0 +1,9 @@
+/**
+ * Sample private AppRegistry resource
+ */
+module.exports = `@prefix solid: <http://www.w3.org/ns/solid/terms#>.
+
+<>
+  a solid:AppRegistry;
+  a solid:UnlistedDocument.
+`
